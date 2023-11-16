68a0afb351040fd7181bdc3b6e6035e9e4f43c58
Alice Figueiredo <alicefigueiredo837@gmail.com>
2023-11-16 17:42:37 -0300

    Fix both scores being written on the same label

--- src/ui.gd
+++ src/ui.gd
@@ -5,10 +5,10 @@ var score := [0, 0]
 @onready var player_one_score_label := $PlayerOneScoreLabel
 @onready var player_two_score_label := $PlayerTwoScoreLabel
 
-func _on_left_goal_body_entered(_body: Node2D) -> void:
+func _on_right_goal_body_entered(_body: Node2D) -> void:
 	score[0] += 1
 	player_one_score_label.text = str(score[0])
 
-func _on_right_goal_body_entered(_body: Node2D) -> void:
+func _on_left_goal_body_entered(_body: Node2D) -> void:
 	score[1] += 1
-	player_one_score_label.text = str(score[1])
+	player_two_score_label.text = str(score[1])
