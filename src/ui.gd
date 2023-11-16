679e1628521a6d5709b79df8ce7aa6b008d16ff4
Alice Figueiredo <alicefigueiredo837@gmail.com>
2023-11-16 17:07:27 -0300

    Add player score to UI

---
+++ src/ui.gd
@@ -0,0 +1,14 @@
+extends HBoxContainer
+
+var score := [0, 0]
+
+@onready var player_one_score_label := $PlayerOneScoreLabel
+@onready var player_two_score_label := $PlayerTwoScoreLabel
+
+func _on_left_goal_body_entered(_body: Node2D) -> void:
+	score[0] += 1
+	player_one_score_label.text = str(score[0])
+
+func _on_right_goal_body_entered(_body: Node2D) -> void:
+	score[1] += 1
+	player_one_score_label.text = str(score[1])
