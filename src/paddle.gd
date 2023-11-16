816b5971065b00c656e8c1edaae5ce1a9d1e47a0
Alice Figueiredo <alicefigueiredo837@gmail.com>
2023-11-16 06:38:11 -0300

    Add both player paddles

---
+++ src/paddle.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed:            int
+@export var player_character: bool
+
+var _direction := Vector2.ZERO
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	_direction = Vector2.ZERO
+	
+	if player_character:
+		_direction.y = Input.get_axis('player-one-up', 'player-one-down')
+	else:
+		_direction.y = Input.get_axis('player-two-up', 'player-two-down')
+
+func _physics_process(delta: float) -> void:
+	move_and_collide(speed * delta * _direction)
